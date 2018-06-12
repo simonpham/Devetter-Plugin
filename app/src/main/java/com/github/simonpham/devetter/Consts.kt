9cf36be0bca4a8d8c38f57b29fd698ce2c764a76
Simon Pham <simonpham.dn@gmail.com>
2018-06-12 15:30:24 +0700

    Add Consts

---
+++ app/src/main/java/com/github/simonpham/devetter/Consts.kt
@@ -0,0 +1,15 @@
+package com.github.simonpham.devetter
+
+/**
+ * Created by Simon Pham on 6/12/18.
+ * Email: simonpham.dn@gmail.com
+ */
+
+const val TYPE_GLOBAL = 0
+const val TYPE_SECURE = 1
+const val TYPE_SYSTEM = 2
+
+const val DATA_TYPE_INT = 0
+const val DATA_TYPE_LONG = 1
+const val DATA_TYPE_FLOAT = 2
+const val DATA_TYPE_STRING = 3
